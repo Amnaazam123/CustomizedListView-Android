1d17607e416968a771f015a162956f2aa81620fd
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-06-02 05:09:10 +0500

    second commit

--- app/src/main/java/com/example/customized_listview/MainActivity.java
+++ app/src/main/java/com/example/customized_listview/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.customized_listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ArrayList<Student> studentArrayList = new ArrayList<Student>();
+        studentArrayList.add(new Student("Ali", "Alpha", "1", R.drawable.a));
+        studentArrayList.add(new Student("Saad", "Beta", "2", R.drawable.b));
+        studentArrayList.add(new Student("Aila", "Alpha", "5", R.drawable.c));
+        studentArrayList.add(new Student("Aiman", "Beta", "6", R.drawable.d));
+        MyAdapter adapter = new MyAdapter(this, studentArrayList);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 }
---
+++ app/src/main/java/com/example/customized_listview/MyAdapter.java
@@ -0,0 +1,39 @@
+package com.example.customized_listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends ArrayAdapter<Student>
+{
+    public MyAdapter(@NonNull Context context, ArrayList<Student> studentArrayList) {
+        super(context, 0, studentArrayList);
+    }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    Student student = getItem(position);
+    convertView = LayoutInflater.from(getContext()).inflate(R.layout.studentview, parent, false);
+    TextView textViewName =  convertView.findViewById(R.id.name);
+    TextView textViewCampus = convertView.findViewById(R.id.section);
+    TextView textViewId = convertView.findViewById(R.id.id);
+    ImageView imageView = convertView.findViewById(R.id.imageView2);
+    textViewName.setText(student.name);
+    textViewCampus.setText(student.campus);
+    textViewId.setText(student.rollNumber);
+    imageView.setImageResource(student.imageID);
+    return convertView;
+    }
+
+
+}
+
